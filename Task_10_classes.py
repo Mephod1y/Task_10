c426305c270c98166804115aa2040c33ea83f030
Mephod1y <73156817+Mephod1y@users.noreply.github.com>
2022-12-08 17:00:46 +0200

    Add files via upload

---
+++ Task_10_classes.py
@@ -0,0 +1,31 @@
+from collections import UserDict
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+    def show_all(self):
+        return self.data
+    def get_data(self, name):
+        return self.data.get(name)
+
+
+class Field():
+    def __init__(self, value):
+        self.value = value
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    pass
+
+class Birthday(Field):
+    pass
+
+class Record():
+    def __init__(self, name, phone, birthday):
+        self.name = Name(name)
+        self.phone = []
+        self.birthday = Birthday(birthday)
+ 
+contacts_dict = AddressBook()
