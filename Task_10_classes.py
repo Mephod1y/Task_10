1ae3ed5705a3d528681512ac010a2639d364ede4
Mephod1y <73156817+Mephod1y@users.noreply.github.com>
2022-12-09 17:47:06 +0200

    Update Task_10_classes.py

--- Task_10_classes.py
+++ Task_10_classes.py
@@ -8,25 +8,35 @@ class AddressBook(UserDict):
     def get_data(self, name):
         return self.data.get(name)
 
-
 class Field():
     def __init__(self, value):
         self.value = value
 
 class Name(Field):
-    pass
+    def __init__(self, value):
+        # super().__init__(name)
+        self.value = value
 
 class Phone(Field):
-    pass
+    def __init__(self, value):
+        # super().__init__(phone)
+        self.value = value
 
 class Birthday(Field):
     pass
 
 class Record():
-    def __init__(self, name):
+    def __init__(self, name, *phones):
         self.name = Name(name)
-        self.phone = []
+        self.phones = []
+        if phones:
+            for phone in phones:
+                self.put_phone_list(phone)
         # self.birthday = Birthday(birthday)
-    def put_phone_list(self, phones):
-        return self.phone.append(Phone(phones))
- 
+    
+    def put_phone_list(self, phone_new):
+        phone_new = Phone(phone_new).value
+        for phone in self.phones:
+            if phone_new == phone.value:
+                print(f"{phone_new} already recorded for {self.name.value}")
+        self.phones.append(Phone(phone_new))
