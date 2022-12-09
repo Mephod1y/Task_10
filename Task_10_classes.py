689a4063da37b9ced9c32267fa3748c12ef2977d
Mephod1y <73156817+Mephod1y@users.noreply.github.com>
2022-12-09 15:08:39 +0200

    Update Task_10_classes.py

--- Task_10_classes.py
+++ Task_10_classes.py
@@ -1,108 +1,32 @@
-from Task_10_classes import AddressBook, Record
+from collections import UserDict
 
-contacts_dict = AddressBook()
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+    def show_all(self):
+        return self.data
+    def get_data(self, name):
+        return self.data.get(name)
 
-def input_error(function):
-    def wrapper(*args, **kwargs):
-        try:
-            return function(*args, **kwargs)
-        except KeyError:
-            return 'Wrong name'
-        except ValueError as exception:
-            return exception.args[0]
-        except IndexError:
-            return 'Enter name and number'
-        except TypeError:
-            return 'Wrong command'
-    return wrapper
 
-@input_error
-def hello():
-    return 'How can I help you?'
+class Field():
+    def __init__(self, value):
+        self.value = value
 
-@input_error
-def exit_func():
-    return "Good bye"
+class Name(Field):
+    pass
 
-@input_error
-def name_phone(data):  # функція перевіряє на коректність введення ім'я телефону
-    new_data = data.strip().split(" ")
-    name = new_data[0]
-    phone = new_data[1]
-    if name.isnumeric():
-        raise ValueError('Wrong name')
-    if not phone.isnumeric():
-        raise ValueError('Wrong phone')
-    return name, phone
+class Phone(Field):
+    pass
 
-@input_error
-def add_name_phone(data):  # функція додає ведене ім'я телефон до екземляру класу Record
-    name, phones = name_phone(data)
-    if name in contacts_dict:
-        raise ValueError('This contact already exist.')
-    # contacts_dict[name] = phone
-    record = Record(name)
-    record.put_phone_list(phones)
-    contacts_dict.add_record(record)
-    return f'You added new contact: {name} with this {phones}'
+class Birthday(Field):
+    pass
 
-@input_error
-def show_all():
-    contacts = ''
-    for record in contacts_dict:
-        contacts += f'{name} : {phone.value}\n'
-    return contacts
-
-@input_error
-def change_phone(data):
-    name, phone = name_phone(data)
-    if name in contacts_dict:
-        contacts_dict[name] = phone
-        return f'You changed number to {phone} for {name}'
-    return 'Use add command'
-
-@input_error
-def show_phone(name):
-    if name.strip() not in contacts_dict:
-        raise ValueError('This contact does not exist')
-    return contacts_dict.get(name.strip())
-
-functions = {
-    'hello': hello,
-    'add': add_name_phone,
-    'change': change_phone,
-    'phone': show_phone,
-    'show all': show_all,
-    'exit': exit_func,
-    'close': exit_func,
-    'good bye': exit_func
-}
-
-def parser_input(user_input):
-    new_input = user_input
-    data = ''
-    for key in functions:
-        if user_input.strip().lower().startswith(key):
-            new_input = key
-            data = user_input[len(new_input):]
-            break
-    if data:
-        return reaction_func(new_input)(data)
-    return reaction_func(new_input)()
-
-def reaction_func(reaction):
-    return functions.get(reaction, break_func)
-
-def break_func():
-    return 'Wrong command'
-
-def main():
-    while True:
-        user_input = input('Enter command: ')
-        result = parser_input(user_input)
-        print(result)
-        if result == 'Good bye':
-            break
-
-if __name__ == "__main__":
-    main()
+class Record():
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phone = []
+        # self.birthday = Birthday(birthday)
+    def put_phone_list(self, phones):
+        return self.phone.append(Phone(phones))
+ 
